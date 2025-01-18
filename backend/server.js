d74f17feb080e2c22e429e3491fdf282785fddf0
Tony <t4tonykuriakose@gmail.com>
2025-01-18 19:01:04 +0530

    auth controller

--- backend/server.js
+++ backend/server.js
@@ -1,19 +1,23 @@
-const express = require("express");
+import express from "express";
+import dotenv from "dotenv";
+import authRoutes from "./routes/auth.routes.js";
+
+dotenv.config();
+
 const app = express();
 
-app.get("/",(req,res)=>{
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
 
-    res.send("Hello World!")
+app.use("/api/auth", authRoutes);
 
-    
+const PORT = 5500;
+app.listen(PORT, () => {
+  console.log("Server running on port", PORT);
+});
 
-})
 
-
-app.listen(3000,()=>{
-    console.log('Server Running');
-    
-})
 
 
 
