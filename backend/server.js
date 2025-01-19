a1f44305b7af310cfe13ad5ab64f1e1d94dc1964
Tony <t4tonykuriakose@gmail.com>
2025-01-19 13:41:27 +0530

    context

--- backend/server.js
+++ backend/server.js
@@ -14,7 +14,7 @@ dotenv.config();
 
 const __dirname = path.resolve();
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 3000;
 
 app.use(express.json()); 
 app.use(cookieParser());
